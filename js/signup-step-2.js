341cffcc541bab9443e4fbfa3b6b957842623f7f
surbhi <surbhisaga337865@gmail.com>
2022-03-15 12:55:17 +0530

    signup error bug fix 1

--- js/signup-step-2.js
+++ js/signup-step-2.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
         var checked = $("#step2form .checkbox:checked").length;
         var isValid = checked > 0;
         $("#spnError")[0].style.display = isValid ? "none" : "block";
-
+        if (isValid >= 1) {
+            window.location = "home.html";
+        }
     });
     $(".checkbox").change(function () {
         var checked = $("#step2form .checkbox:checked").length;
